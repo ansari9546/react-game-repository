31092e0865a9173a7d9f2ee2df1d52a3db6c661e
arsalan <arsalan@onescreensolutions.com>
2023-06-06 14:14:31 +0500

    Added platfomrs icons

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -1,5 +1,6 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 import { Games } from "../../hooks/useGames";
+import PlatFormIconList from "./PlatformIconList";
 
 interface Props {
     game: Games
@@ -11,6 +12,7 @@ export default function GamesCard({ game }: Props) {
             <Image src={game.background_image}></Image>
             <CardBody>
                 <Heading fontSize={'2xl'}>{game.name}</Heading>
+                <PlatFormIconList platforms={game.parent_platforms.map(p => p.platform)}></PlatFormIconList>
             </CardBody>
         </Card>
     )
